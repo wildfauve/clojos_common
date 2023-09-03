b2c345eff378d7ac71e22722d97f2fe73d0c7308
Col Perks <wild.fauve@gmail.com>
2023-09-04 11:52:36 +1200

    add new tokeniser

--- clojos_common/util/tokeniser.py
+++ clojos_common/util/tokeniser.py
@@ -11,3 +11,7 @@ def string_tokeniser(in_str: str, str_splitter: re.Pattern, char_replacers: List
     return fn.multi_replace(str_splitter.split(in_str)[-1], char_replacers)
 
 
+
+def titleiser_tokeniser(in_str: str, str_splitter: re.Pattern, char_replacers: List) -> str:
+    return "".join(list(map(lambda token: fn.multi_replace(token, char_replacers).title(), str_splitter.split(in_str))))
+
--- tests/test_util.py
+++ tests/test_util.py
@@ -1,4 +1,11 @@
 from clojos_common.util import tokeniser
 
+
 def test_tokenise():
-    assert tokeniser.string_tokeniser("Bronzie.Bear", tokeniser.sp_splitter, tokeniser.special_char_set) == "Bronzie_Bear"
+    assert tokeniser.string_tokeniser("Bronzie.Bear", tokeniser.sp_splitter,
+                                      tokeniser.special_char_set) == "Bronzie_Bear"
+
+
+def test_titleiser():
+    assert tokeniser.titleiser_tokeniser("bronzie bear", tokeniser.sp_splitter,
+                                         tokeniser.special_char_set) == "BronzieBear"
